12ae99115965200d61260c0957262d0c86d60082
Josh-MITT <jfehr1@student.mitt.ca>
2026-02-24 11:10:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const API_BASE = "https://hydro-tracker-api-nsa-gjgtdch6gydme8fu.eastus-01.azurewebsites.net";
+
+function callBackend() {
+  fetch(API_BASE_URL + "/api/health")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("output").textContent =
+        JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+      document.getElementById("output").textContent =
+        "Error: " + error;
+    });
